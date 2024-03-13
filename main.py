e190101c1b5ec9223a1ba68936ae084aabb499a1
Luis Eduardo Craizer <lecraizer@tecgraf.puc-rio.br>
2024-03-13 13:39:44 -0300

    added cuda:0

--- main.py
+++ main.py
@@ -128,7 +128,7 @@ print(len(df_features))
 start_time = time.time()
 print('Cuda avaiable?', torch.cuda.is_available())
 # device = torch.device('cuda:2') if torch.cuda.is_available() else torch.device('cpu')
-device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
 
 
 # # Random Forest training
